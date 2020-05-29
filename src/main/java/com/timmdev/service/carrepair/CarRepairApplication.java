926ed203640c94f90ab21f66ce9469b7e94ffe35
TimurMuratov <yeicauM2ua4c>
2020-05-30 02:41:44 +0400

    Initial commit

---
+++ src/main/java/com/timmdev/service/carrepair/CarRepairApplication.java
@@ -0,0 +1,13 @@
+package com.timmdev.service.carrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRepairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timmdev/service/carrepair/CarRepairApplicationTests.java
@@ -0,0 +1,9 @@
+package com.timmdev.service.carrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRepairApplicationTests {
+
+}
